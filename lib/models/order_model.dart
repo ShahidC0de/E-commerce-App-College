aff73de47a1d842651459cf43d648db8d1f542cd
Shahid-Zada <Shahidzada.cs@gmail.com>
2024-02-21 22:16:58 +0500

    remove un-necessary imports

--- lib/models/order_model.dart
+++ lib/models/order_model.dart
@@ -1,6 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:tech_trove_shop/models/product_model.dart';
 
 class OrderModel {
