b2ad3f7e187a63e2648e468fc58c3f0a7aeb7537
ShahidC0de <shahidzada.cs@gmail.com>
2024-11-05 16:47:46 +0500

    removing spare dependencies

--- lib/Firebase/firebase_firestore.dart
+++ lib/Firebase/firebase_firestore.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: use_build_context_synchronously
 
-import 'dart:convert';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
